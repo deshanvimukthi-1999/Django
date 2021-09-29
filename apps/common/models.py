69a4426e1298ee3d11c9c52657ef98f3636cb086
Deshan Vimukthi <vimudeshan123@gmail.com>
2021-09-29 11:27:41 +0530

    New changes

--- apps/common/models.py
+++ apps/common/models.py
@@ -10,30 +10,31 @@ class Company(models.Model):
     user = models.ForeignKey(User, related_name='owner', on_delete=models.CASCADE)
 
 
-class Question(models.Model):
-    question_type = models.CharField(max_length=100)
-    documents = models.CharField
+class Job(models.Model):
+    job_role = models.CharField(max_length=200)
+    company = models.ForeignKey(Company, related_name='jobs', on_delete=models.CASCADE)
 
 
 class Section(models.Model):
     name = models.CharField(max_length=200)
-    question = models.ForeignKey(Question, related_name='questions', on_delete=models.CASCADE)
-    company = models.ForeignKey(Company, related_name='company', on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, related_name='sections', on_delete=models.CASCADE)
 
 
-class Job(models.Model):
-    job_role = models.CharField(max_length=200)
-    company = models.ForeignKey(Company, related_name='company', on_delete=models.CASCADE)
-    section = models.ForeignKey(Section, related_name='section', on_delete=models.CASCADE)
+class Question(models.Model):
+    question_type = models.CharField(max_length=100)
+    documents = models.CharField
+    section = models.ForeignKey(Section, related_name='questions', on_delete=models.CASCADE)
 
 
 class Assessor(models.Model):
     name = models.CharField(max_length=200)
     documents = models.CharField(max_length=200)
+    user = models.ForeignKey(User, related_name='assessors', on_delete=models.CASCADE)
 
 
 class Interview(models.Model):
-    assessor = models.ForeignKey(Assessor, related_name='assessor', on_delete=models.CASCADE)
+    assessor = models.ForeignKey(Assessor, related_name='interviews', on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, related_name='interviews', on_delete=models.CASCADE)
 
 
 class Candidate(models.Model):
@@ -46,7 +47,7 @@ class Experience(models.Model):
     placed_work = models.CharField(max_length=100)
     time_period = models.DateTimeField
     role = models.CharField(max_length=100)
-    candidate = models.ForeignKey(Candidate, related_name='candidate', on_delete=models.CASCADE)
+    candidate = models.ForeignKey(Candidate, related_name='experiences', on_delete=models.CASCADE)
 
 
 
